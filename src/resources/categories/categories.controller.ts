21bc42ccdaad6c8c078dbcb9296c3948305a466d
Julio Torres <jtorres990@gmail.com>
2025-06-23 15:18:12 -0500

    refactor(categories): improve locale handling and relations setup in findAll method

--- src/resources/categories/categories.controller.ts
+++ src/resources/categories/categories.controller.ts
@@ -12,14 +12,18 @@ export class CategoriesController {
   @Get()
   async findAll(@Query() query: QueryParams<Category>) {
     const options = buildFindManyOptions(query);
-    const locale = query.locale || 'en';
-    options.relations = ['translations'];
-    options.where = {
-      ...options.where,
-      translations: {
-        locale,
-      },
-    };
+    const locale = query.locale;
+    options.relations = options.relations ?? {};
+    const relations = (options.relations as string[]) || [];
+    if (locale) {
+      relations.push('translations');
+      options.where = {
+        ...options.where,
+        translations: {
+          locale,
+        },
+      };
+    }
     return this.categoriesService.findAll(options);
   }
 
